55d9f96fda02a51561bfde1c76368e89e02f7d97
Jrperez79 <joelrperez@gmail.com>
2020-08-03 17:27:40 -0700

    joels initial commit

---
+++ data.js
@@ -0,0 +1,16 @@
+
+export const ingredients = [
+    {ingredients: '1 1/2 cups Milk'},
+    {ingredients: '1/2 cup mascarpone'},
+    {ingredients: '1/2 tsp pink salt'},
+    {ingredients: '1 lb Black Mission Figs'},
+    {ingredients: '1/2 cup brown sugar'},
+    {ingredients: '2-4 tbsp water'},
+    {ingredients: '1/2 cups heavy cream'},
+    {ingredients: '1/3 granulated sugar'},
+    {ingredients: '2 egg yolks'},
+    {ingredients: '1 lemon, juiced'},
+    {ingredients: '2 tbsp butter'},
+    {ingredients: '1 cup honey roasted pecans, roughly chopped'}
+]
+
